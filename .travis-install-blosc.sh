05e740303fbb42dfe35ac304a46ef7c8095d6265
Christian Angelsen <christian.angelsen@stfc.ac.uk>
2021-04-19 12:21:18 +0100

    Implements GitHub Actions on data plane
        Ensures data integration (odinDataTest) runs on hexitec's data plane
        Fixes 2 regressions: raw_frames still defined as float in some config files, HexitecThresholdPlugin lost a member variable.

--- .travis-install-blosc.sh
+++ .travis-install-blosc.sh
@@ -3,13 +3,13 @@ set -e
 BLOSC=c-blosc-1.14.2
 
 # check to see if blosc folder is empty
-if [ ! -d "$HOME/${BLOSC}/lib" ]; then
+if [ ! -d "$RUNNER_WORKSPACE/hexitec-detector/${BLOSC}/lib" ]; then
   curl --output ${BLOSC}.tar.gz -L https://codeload.github.com/Blosc/c-blosc/tar.gz/v1.14.2;
   tar -zxf ${BLOSC}.tar.gz;
   cd ${BLOSC};
   mkdir build;
   cd build;
-  cmake -DCMAKE_INSTALL_PREFIX=$HOME/$BLOSC ..;
+  cmake -DCMAKE_INSTALL_PREFIX=$RUNNER_WORKSPACE/hexitec-detector/$BLOSC ..;
   cmake --build . --target install;
 else
   echo 'Using cached directory.';
--- .travis-install-odin.sh
+++ .travis-install-odin.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ODIN="odin-data"
-ODIN_PREFIX=$HOME/$ODIN
+ODIN_PREFIX=$RUNNER_WORKSPACE/hexitec-detector/$ODIN
 git clone https://github.com/odin-detector/odin-data.git
 cd odin-data
 git checkout master
 mkdir -p build;
 cd build;
 cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT -DBLOSC_ROOT_DIR=$BLOSC_ROOT ..
-make -j 8 VERBOSE=1
+make -j 2 VERBOSE=1
 make install
