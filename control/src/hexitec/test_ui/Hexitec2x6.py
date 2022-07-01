8c4829c0e0710d587d18f660d6dc3f8149076d26
Christian Angelsen <christian.angelsen@stfc.ac.uk>
2022-07-01 14:55:12 +0100

    Able to read/write via 10G channel
        Brittle, easily locks up without pausing before/after write.

--- control/src/hexitec/test_ui/Hexitec2x6.py
+++ control/src/hexitec/test_ui/Hexitec2x6.py
@@ -1,11 +1,11 @@
 """
-HexitecFEM for Hexitec ODIN control.
+Hexitec2x6: Exercises UDP control plane.
 
-Christian Angelsen, STFC Detector Systems Software Group, 2019.
+Christian Angelsen, STFC Detector Systems Software Group, 2022.
 """
 
 from RdmaUDP import RdmaUDP
-
+import time
 
 class Hexitec2x6():
     """
@@ -17,17 +17,15 @@ class Hexitec2x6():
     def __init__(self, debug=False):
         self.debug = debug
         # Control IP addresses
-        self.server_ctrl_ip_addr = "10.0.3.1" # Network card
-        self.camera_ctrl_ip_addr = "10.0.3.2" # Hexitec 2x6 interface
+        self.server_ctrl_ip_addr = "10.0.3.1"  # Network card
+        self.camera_ctrl_ip_addr = "10.0.3.2"  # Hexitec 2x6 interface
         self.x10g_rdma = None
 
     def __del__(self):
         self.x10g_rdma.close()
 
     def connect(self):
-        # # SRC UDP Port: 61648
-        # # DST UDP Port: 61649
-        # write_axi 0x2002C 0xF0D1F0D0
+        """Connect to the 10 G UDP control channel."""
         self.x10g_rdma = RdmaUDP(self.server_ctrl_ip_addr, 61649, self.server_ctrl_ip_addr, 61648,
                                  self.camera_ctrl_ip_addr, 61648, 2000000, 9000, 20, self.debug)
         self.x10g_rdma.setDebug(self.debug)
@@ -35,30 +33,106 @@ class Hexitec2x6():
         return self.x10g_rdma.error_OK
 
     def read_scratch_registers(self):
-        # self.x10g_rdma.write(0x8030, 0xAAA00555, "New Scratch Register1 value")
-        # Read the Scratch registers:
+        """Read scratch registers."""
         scratch0 = self.x10g_rdma.read(0x00008030, 'Read Scratch Register 1')
-        print("Scratch: 0x{0:08x}".format(scratch0))
-        # scratch1 = self.x10g_rdma.read(0x00008038, 'Read Scratch Register 2')
-        # scratch2 = self.x10g_rdma.read(0x0000803C, 'Read Scratch Register 3')
-        # print("Scratch: 0x{0:08x} {1:08x} {2:08x}".format(scratch2, scratch1, scratch0))
+        scratch1 = self.x10g_rdma.read(0x00008034, 'Read Scratch Register 2')
+        scratch2 = self.x10g_rdma.read(0x00008038, 'Read Scratch Register 3')
+        scratch3 = self.x10g_rdma.read(0x0000803C, 'Read Scratch Register 4')
+        print("Scratch: 0x{0:08x} {1:08x} {2:08x} {3:08X}".format(scratch3, scratch2, scratch1, scratch0))
+
+    def write_scratch_registers(self):
+        """Write values to the four scratch registers."""
+        # self.x10g_rdma.write(0x8030, 0x12345678, "New Scratch Register1 value")
+        time.sleep(1)
+        self.x10g_rdma.write(0x8034, 0x9A0000F1, "New Scratch Register2 value")
+        time.sleep(1)
+        # self.x10g_rdma.write(0x8038, 0xAAAAAAAA, "New Scratch Register3 value")
+        # time.sleep(1)
+        # self.x10g_rdma.write(0x803C, 0x60054003, "New Scratch Register4 value")
+        # time.sleep(1)
 
     def read_fpga_dna_registers(self):
-        # Read the three DNA registers:
+        """Read the three DNA registers."""
         fpga_dna0 = self.x10g_rdma.read(0x0000800C, 'Read FPGA DNA part 1')
         fpga_dna1 = self.x10g_rdma.read(0x00008010, 'Read FPGA DNA part 2')
         fpga_dna2 = self.x10g_rdma.read(0x00008014, 'Read FPGA DNA part 3')
         print("FPGA DNA: 0x{0:08x} {1:08x} {2:08x}".format(fpga_dna2, fpga_dna1, fpga_dna0))
 
     def disconnect(self):
+        """."""
         self.x10g_rdma.close()
+
+# Testing functionality for Matt Robert 's scripting
+def prod_list(bytes):
+    """."""
+    bytes_list = []
+    for idx in range(len(bytes)):
+        bytes_list.append(bytes[idx])
+    return bytes_list
+
+def display_register_information(name, read_bytes):
+    r"""Take a bytes object, display its name, length, address and value.
+
+    I.e. b"\x02\x00\x00\x01\x04\x00\x02\x00\x00b\x00\x00'\xb8`\xb4"
+    Contain address: 0x00020004, Value: 0x00006200, Length: 16
+    """
+    bytes_list = prod_list(read_bytes)
+    print("Reg: {}, length: {}".format(name, len(read_bytes)))
+    print("     address: 0x{0:02x}{1:02x}{2:02x}{3:02x}".format(bytes_list[7], bytes_list[6], bytes_list[5], bytes_list[4]))
+    print("       Value: 0x{0:02x}{1:02x}{2:02x}{3:02x}".format(bytes_list[11], bytes_list[10], bytes_list[9], bytes_list[8]))
 
 
 if __name__ == '__main__':  # pragma: no cover
 
     hxt = Hexitec2x6(True)
     hxt.connect()
+    # hxt.read_scratch_registers()
+    hxt.write_scratch_registers()
     hxt.read_scratch_registers()
     # hxt.read_fpga_dna_registers()
     hxt.disconnect()
     print("All done!")
+
+    # A few example bytes objects, converted into human readable format
+
+    # # #Scratch register 1
+    # # #*** READ, Going to send:
+    # # #b'\x02\x00\x00\x010\x80\x00\x00'
+    # # #Read Back Data:
+    # sr1 =	b'\x02\x00\x00\x010\x80\x00\x00\x11\x11\x11\x11\xef\xbe\xad\xde'
+    # display_register_information("sr1", sr1)
+
+    # # #Scratch Register 2
+    # # #*** READ, Going to send:
+    # # # b'\x02\x00\x00\x014\x80\x00\x00'
+    # # # Read Back Data:
+    # sr2 = b'\x02\x00\x00\x014\x80\x00\x00\xef\xbe\xad\xde3333'
+    # display_register_information("sr2", sr2)
+
+    # # # Scratch Register 3
+    # # # *** READ, Going to send:
+    # # # b'\x02\x00\x00\x018\x80\x00\x00'
+    # # # Read Back Data:
+    # sr3 = b'\x02\x00\x00\x018\x80\x00\x003333DDDD'
+    # display_register_information("sr3", sr3)
+
+    # # # Scratch Register 4
+    # # # *** READ, Going to send:
+    # # # b'\x02\x00\x00\x01<\x80\x00\x00'
+    # # # Read Back Data:
+    # sr4 = b'\x02\x00\x00\x01<\x80\x00\x00DDDD\x00\x00\x00\x00'
+    # display_register_information("sr4", sr4)
+
+    # # Source Mac address, lower
+    # # *** READ, Going to send:
+    # # b'\x02\x00\x00\x01\x00\x00\x02\x00'
+    # # Read Back Data:
+    # sml = b'\x02\x00\x00\x01\x00\x00\x02\x00\x04\x00\x00\x00\x00b\x00\x00'
+    # display_register_information("sml", sml)
+
+    # # Source Mac address, upper
+    # # *** READ, Going to send:
+    # # b'\x02\x00\x00\x01\x04\x00\x02\x00'
+    # # Read Back Data:
+    # smu = b"\x02\x00\x00\x01\x04\x00\x02\x00\x00b\x00\x00'\xb8`\xb4"
+    # display_register_information("smu", smu)
--- control/src/hexitec/test_ui/RdmaUDP.py
+++ control/src/hexitec/test_ui/RdmaUDP.py
@@ -14,30 +14,15 @@ class RdmaUDP(object):
         self.debug = debug
         if self.debug:
             print("RdmaUDP:")
-            print("  RDMA IP addresses")
-            print("	\trxsocket.bind({}, {})".format(MasterRxUDPIPAddress, MasterRxUDPIPPort))
-            print("	\ttxsocket.bind({}, {})".format(MasterTxUDPIPAddress, MasterTxUDPIPPort))
-
-            print("  Target, read & write target this through txsocket")
-            print("	\tTargetRxUDPIPAddress	{}".format(TargetRxUDPIPAddress))
-            print("	\tTargetRxUDPIPPort   	{}".format(TargetRxUDPIPPort))
+            print("	Binding to:   ({}, {})".format(MasterTxUDPIPAddress, MasterTxUDPIPPort))
+            print(" Transmitting: ({}, {})".format(TargetRxUDPIPAddress, TargetRxUDPIPPort))
             print("___________________________________________________________ ")
 
         self.txsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.rxsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.rxsocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, RxUDPBuf)
         timeval = struct.pack('ll', 5, 100)
-        self.rxsocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeval)
+        self.txsocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeval)
         self.error_OK = True
-
-        try:
-            self.rxsocket.bind((MasterRxUDPIPAddress, MasterRxUDPIPPort))
-        except socket.error as e:
-            error_string = "  Error: '{}' ".format(e)
-            error_string += "on RX Socket: {}:{}".format(MasterRxUDPIPAddress, MasterRxUDPIPPort)
-            print(error_string)
-            self.error_OK = False
 
         try:
             self.txsocket.bind((MasterTxUDPIPAddress, MasterTxUDPIPPort))
@@ -47,30 +32,27 @@ class RdmaUDP(object):
             print(error_string)
             self.error_OK = False
 
-        self.rxsocket.setblocking(1)
-
         self.TgtRxUDPIPAddr = TargetRxUDPIPAddress
         self.TgtRxUDPIPPrt = TargetRxUDPIPPort
-
         self.UDPMaxRx = UDPMTU
-
         self.ack = False
 
     def __del__(self):
         self.txsocket.close()
-        self.rxsocket.close()
 
     def read(self, address, comment=''):
-        command = struct.pack('=BBBBIQBBBBIQQQQQ', 1, 0, 0, 3, address, 0, 9, 0, 0,
-                              255, 0, 0, 0, 0, 0, 0)
+        command = struct.pack('=BBBBI', 2, 0, 0, 1, address)
+        # print("Going to send the command: ({})".format(type(command)));print(command)#;return 0
         self.txsocket.sendto(command, (self.TgtRxUDPIPAddr, self.TgtRxUDPIPPrt))
-
+        print("read, acknowledged: ", self.ack)
         if self.ack:
-            response = self.rxsocket.recv(self.UDPMaxRx)
+            response = self.txsocket.recv(self.UDPMaxRx)
             data = 0x00000000
-            if len(response) == 56:
-                decoded = struct.unpack('=IIIIQQQQQ', response)
-                data = decoded[3]
+            if len(response) == 16:
+                decoded = struct.unpack('=BBBBIII', response)
+                data = decoded[5]
+            else:
+                print(" Read ack Error! Response (length:{}) != 16".format(len(response)))
 
         if self.debug:
             print('R %08X : %08X %s' % (address, data, comment))
@@ -81,70 +63,23 @@ class RdmaUDP(object):
         if self.debug:
             print('W %08X : %08X %s' % (address, data, comment))
 
-        # Create single write command + 5 data cycle nop command for padding
-        command = struct.pack('=BBBBIQBBBBIQQQQQ', 1, 0, 0, 2, address,
-                              data, 9, 0, 0, 255, 0, 0, 0, 0, 0, 0)
-
+        command = struct.pack('=BBBBII', 1, 0, 0, 0, address, data)
+        # print("Going to send the (Write) command: ({})".format(type(command)));print(command)#;return 0
         # Send the single write command packet
         self.txsocket.sendto(command, (self.TgtRxUDPIPAddr, self.TgtRxUDPIPPrt))
-
+        print("writing, acknowledged: ", self.ack)
         if self.ack:
             # Receive acknowledge packet
-            # response = self.rxsocket.recv(self.UDPMaxRx)
-            _ = self.rxsocket.recv(self.UDPMaxRx)
+            response = self.txsocket.recv(self.UDPMaxRx)
             # TODO: Remove as redundant?
-            # if len(response) == 48:
-            #     decoded = struct.unpack('=IIIIQQQQ', response)
-            #     print(decoded)
-
-    def block_read(self, address, length, comment=''):
-        length = length // 4 - 1
-        command = struct.pack('=BBBBI', length, 0, 0, 0, 1, address)
-
-        self.txsocket.sendto(command, (self.TargetRxUDPIPAddr, self.TargetRxUDPIPPrt))
-
-        if self.ack:
-            response = self.rxsocket.recv(self.UDPMaxRx)
-
-        return response
-
-    def block_write(self, address, data, comment=''):
-
-        if self.debug:
-            print('W %08X : %08X %s' % (address, data, comment))
-
-        # Create block write command
-        length = len(data) // 4 - 1
-        command = struct.pack('=BBBBI', length, 0, 0, 0, address)
-        command = command + data
-        print(len(command))
-
-        # Send the single write command packet
-        self.txsocket.sendto(command, (self.TgtRxUDPIPAddr, self.TgtRxUDPIPPrt))
-
-        if self.ack:
-            # Receive acknowledge packet
-            response = self.rxsocket.recv(self.UDPMaxRx)
-            print(len(response))
-
-    def block_nop(self, comment=''):
-        if self.debug:
-            print('W %08X : %08X %s' % (comment))
-
-        # Create block nop command
-        command = struct.pack('=BBBBIQQQQQ', 255, 0, 0, 4, 0, 0, 0, 0, 0, 0)
-
-        # Send the single write command packet
-        self.txsocket.sendto(command, (self.TargetRxUDPIPAddr, self.TargetRxUDPIPPrt))
-
-        if self.ack:
-            # Receive acknowledge packet
-            response = self.rxsocket.recv(self.UDPMaxRx)
-            print(len(response))
+            if len(response) == 16:
+                decoded = struct.unpack('=BBBBIII', response)
+                print("Ack: %08X : %08X " % (decoded[4], decoded[5]))
+            else:
+                print("Write ack Error! Response (length: {}) != 16".format(len(response)))
 
     def close(self):
         self.txsocket.close()
-        self.rxsocket.close()
 
     def setDebug(self, enabled=True):
         self.debug = enabled
